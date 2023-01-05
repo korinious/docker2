91076d9430e532e7557135166c10b28b50465ba8
korinious <korinakourkouta@gmail.com>
2023-01-05 19:17:43 +0200

    made some changes in py file

--- docker2.py
+++ docker2.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis, RedisError
 import os
 import socket
-redis = Redis(host="docker-redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+redis = Redis(host="redis-server", db=0, socket_connect_timeout=2, socket_timeout=2)
 app = Flask(__name__)
 @app.route("/")
 def hello():
@@ -12,7 +12,7 @@ def hello():
         visits = "<i>Ooops! Connection could not be established!</i>"
 html = "<h3>Yeah! Docker leme kai (den) klaime! Ok, isws.</h3>\n" \
            "<b>Hostname:</b> {hostname}<br/>\n" \
-           "<b>How many times:</b> {visits}\n"
+           "<b>How many times have you visited:</b> {visits}\n"
 return html.format(hostname=socket.gethostname(), visits=visits)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
